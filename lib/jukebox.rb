9638fd8a5a46ee84b941661a671d77a35a8589cf
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-22 12:15:24 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,64 @@
-# Add your code here
+# help
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+# play
+def play(songs)
+  
+  puts "Please enter a song name or number:"
+  user_response = gets.chomp 
+  
+  if user_response == songs
+  
+  songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+end
+
+
+#list 
+def list
+  count = 1
+  while songs[count] do 
+    p "#{count}. #{song[count]}"
+    count += 1
+  end
+end
+
+
+#exit jukebox
+def exit_jukebox
+  
+end
+
+
+#run 
+def run
+  
+end
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
